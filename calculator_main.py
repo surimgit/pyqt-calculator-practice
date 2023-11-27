b43863a658673a65bbc249ce9beaf8416798d5ea
surimgit <ruriaim@naver.com>
2023-11-27 14:28:06 +0900

    mergeline #1

--- calculator_main.py
+++ calculator_main.py
@@ -15,14 +15,11 @@ class Main(QDialog):
         layout_equation_solution = QFormLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        label_equation = QLabel("Equation: ")
-        label_solution = QLabel("Solution: ")
-        self.equation = QLineEdit("")
-        self.solution = QLineEdit("")
+        label_equation = QLabel()
+        self.equation = QLineEdit()
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
         layout_equation_solution.addRow(label_equation, self.equation)
-        layout_equation_solution.addRow(label_solution, self.solution)
 
         ### 사칙연상 버튼 생성
         button_plus = QPushButton("+")
@@ -104,11 +101,11 @@ class Main(QDialog):
     def button_equal_clicked(self):
         equation = self.equation.text()
         solution = eval(equation)
-        self.solution.setText(str(solution))
+        self.equation.setText(str(solution))
 
     def button_clear_clicked(self):
         self.equation.setText("")
-        self.solution.setText("")
+        self.equation.setText("")
 
     def button_backspace_clicked(self):
         equation = self.equation.text()
